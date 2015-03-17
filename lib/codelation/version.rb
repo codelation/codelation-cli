d5bf1690649a8bd7bcea60f5e6d8a4994ea1ea54
Brian Pattison <brian@brianpattison.com>
2015-03-17 12:41:50 -0500

    Update version to 0.0.14

--- lib/codelation/version.rb
+++ lib/codelation/version.rb
@@ -3,7 +3,7 @@ require "open-uri"
 require "thor"
 
 module Codelation
-  VERSION = "0.0.13"
+  VERSION = "0.0.14"
 
   class Cli < Thor
     desc "update", "Update codelation-cli to latest version"
