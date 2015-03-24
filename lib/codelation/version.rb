69776d16960e47829c124d1193916c84a465f60d
Brian Pattison <brian@brianpattison.com>
2015-03-24 09:46:03 -0500

    Up version to 0.0.17

--- lib/codelation/version.rb
+++ lib/codelation/version.rb
@@ -3,7 +3,7 @@ require "open-uri"
 require "thor"
 
 module Codelation
-  VERSION = "0.0.16"
+  VERSION = "0.0.17"
 
   class Cli < Thor
     desc "update", "Update codelation-cli to latest version"
