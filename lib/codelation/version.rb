b2d2f47739a2fde0a7087a3edc9c2d095ea4ea21
Brian Pattison <brian@brianpattison.com>
2016-02-25 11:16:19 -0600

    Update rubocop.yml

--- lib/codelation/version.rb
+++ lib/codelation/version.rb
@@ -3,7 +3,7 @@ require "open-uri"
 require "thor"
 
 module Codelation
-  VERSION = "0.0.24".freeze
+  VERSION = "0.0.25".freeze
 
   class Cli < Thor
     desc "update", "Update codelation-cli to latest version"
