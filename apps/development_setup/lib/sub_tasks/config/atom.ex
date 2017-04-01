674b3478348b639ca4ce7dd3be06e90959b0744d
Jake Humphrey <jake@codelation.com>
2017-04-01 17:35:50 -0500

    Adding keymap file

--- apps/development_setup/lib/sub_tasks/config/atom.ex
+++ apps/development_setup/lib/sub_tasks/config/atom.ex
@@ -14,5 +14,33 @@ defmodule DevelopmentSetup.Config.Atom do
 
     atom_contents = atom_contents <> "\n# Add Ruby executables to Atom's PATH\nprocess.env.PATH += \":#{DevelopmentSetup.Other.Ruby.ruby_bin_path()}\"\n"
     CommandTools.write_file!(atom_file, atom_contents)
+
+    key_map_file_path = Path.join([System.user_home, ".atom", "keymap.cson"])
+
+    if File.exists?(key_map_file_path) do
+      if CommandTools.prompt?("Atom Key map file already exists. Overwrite it?", force) do
+        CommandTools.write_file!(key_map_file_path, keymap_file())
+      end
+    else
+      CommandTools.write_file!(key_map_file_path, keymap_file())
+    end
+  end
+
+  defp keymap_file do
+    "
+
+'.platform-darwin':
+  'cmd-t': 'fuzzy-finder:toggle-buffer-finder'
+  'shift-cmd-t': 'fuzzy-finder:toggle-file-finder'
+  'cmd-m':'project-manager:list-projects'
+  'cmd-g':'git-plus:menu'
+
+'atom-workspace atom-text-editor:not([mini])':
+  'cmd-j':'pretty-json:prettify'
+
+'.platform-darwin atom-workspace':
+  'cmd-shift-j':'pretty-json:minify'
+
+    "
   end
 end
