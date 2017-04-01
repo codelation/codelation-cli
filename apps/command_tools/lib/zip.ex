a65cadcc218f433ff3baaccae4f07c9b884bdafd
Jake Humphrey <jake@codelation.com>
2017-04-01 16:18:17 -0500

    Fixing unzip stuff

--- apps/command_tools/lib/zip.ex
+++ apps/command_tools/lib/zip.ex
@@ -1,15 +1,9 @@
 defmodule CommandTools.Zip do
   def unzip(filename) do
-    filename
-    |> String.to_char_list
-    |> :zip.unzip
+    System.cmd("unzip", ["-o", filename])
   end
 
   def unzip(filename, to) do
-    filename
-    |> String.to_char_list
-    |> :zip.unzip([:memory])
-    |> (fn({:ok, data}) -> data end).()
-    |> Enum.map(fn({filename, filedata}) -> CommandTools.FileP.write!(Path.join(to, filename), filedata) end)
+    System.cmd("unzip", ["-o", filename, "-d", to])
   end
 end
