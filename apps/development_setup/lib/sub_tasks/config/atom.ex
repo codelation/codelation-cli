cff585e553ae9d538d03387d760298628cbc7106
Jake Humphrey <jake@codelation.com>
2017-04-01 17:24:12 -0500

    Fixing path issue

--- apps/development_setup/lib/sub_tasks/config/atom.ex
+++ apps/development_setup/lib/sub_tasks/config/atom.ex
@@ -1,10 +1,9 @@
 defmodule DevelopmentSetup.Config.Atom do
-  @atom_file Path.join([System.user_home, ".atom", "init.coffee"])
 
   def install(_force) do
-
-    atom_contents = if File.exists?(@atom_file) do
-      {:ok, data} = File.read(@atom_file)
+    atom_file = Path.join([System.user_home, ".atom", "init.coffee"])
+    atom_contents = if File.exists?(atom_file) do
+      {:ok, data} = File.read(atom_file)
       data
     else
       ""
@@ -14,6 +13,6 @@ defmodule DevelopmentSetup.Config.Atom do
     |> String.replace(~r/\nprocess\.env\.PATH.*\n/, "")
 
     atom_contents = atom_contents <> "\n# Add Ruby executables to Atom's PATH\nprocess.env.PATH += \":#{DevelopmentSetup.Other.Ruby.ruby_bin_path()}\"\n"
-    CommandTools.write_file!(@atom_file, atom_contents)
+    CommandTools.write_file!(atom_file, atom_contents)
   end
 end
